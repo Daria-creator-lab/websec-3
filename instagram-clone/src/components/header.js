d972c01f5241a151abb75729f929a8b9d3142a77
PavelDenisov1 <paveldenisovqwe01@mail.ru>
2022-12-20 23:35:48 +0400

    Added a logo to the header.

--- instagram-clone/src/components/header.js
+++ instagram-clone/src/components/header.js
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+
 export default function Header() {
-    return <p>I am the header</p>;
+  return (
+    <div className="h-16 bg-white">
+      <div className="container mx-auto max-w-screen-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="flex items-center">
+            <Link to={ROUTES.DASHBOARD}>
+                <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
