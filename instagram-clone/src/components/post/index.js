ae3b2611fba1c98d893b8b702bdc6a7100aed613
PavelDenisov1 <paveldenisovqwe01@mail.ru>
2022-12-22 05:33:17 +0400

    Created a userPhotos hook

---
+++ instagram-clone/src/components/post/index.js
@@ -0,0 +1,4 @@
+export default function Post({ content }) {
+    console.log('content', content);
+    return <p>I am a post</p>;
+}
--- instagram-clone/src/components/timeline.js
+++ instagram-clone/src/components/timeline.js
@@ -1,3 +1,19 @@
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
+
 export default function Timeline() {
-    return <p>I am the timeline</p>;
+  const { photos } = usePhotos();
+ 
+  return (
+    <div className="container col-span-2">
+      {!photos ?(
+        <p className='font-bold'>loading...</p>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)          
+      ) : (
+        <p className="text-center text-2xl">Follow people to see Photos</p>          
+      )}
+    </div>
+  );
 }
+
